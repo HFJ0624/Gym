a5a950dd6de3b888af213e54aef2f41af5d9da30
黄锋杰 <342586916@qq.com>
2026-03-17 21:12:05 +0800

    前台系统的登录功能实现

---
+++ Gym-parent/Gym-admin/src/main/java/com/sau/gym/admin/controller/FrontIndexController.java
@@ -0,0 +1,55 @@
+package com.sau.gym.admin.controller;
+
+import com.sau.gym.admin.service.MenuService;
+import com.sau.gym.admin.service.UserService;
+import com.sau.gym.admin.service.ValidateCodeService;
+import com.sau.gym.common.log.annotation.Log;
+import com.sau.gym.common.log.enums.OperatorType;
+import com.sau.gym.model.dto.system.LoginDto;
+import com.sau.gym.model.entity.base.Result;
+import com.sau.gym.model.entity.base.ResultCodeEnum;
+import com.sau.gym.model.entity.user.User;
+import com.sau.gym.model.vo.system.LoginVo;
+import com.sau.gym.model.vo.system.ValidateCodeVo;
+import com.sau.gym.utils.AuthContextUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 作者:hfj
+ * 功能:
+ * 日期: 2026/3/17 17:16
+ */
+@RestController
+@RequestMapping(value = "/front")
+public class FrontIndexController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ValidateCodeService validateCodeService;
+
+    //前台登录后台
+    @Log(title = "前台登录",businessType = 0,operatorType = OperatorType.MANAGE)
+    @Operation(summary = "登录接口")
+    @PostMapping(value = "/login")
+    public Result<LoginVo> login(@RequestBody LoginDto loginDto){
+        LoginVo loginVo = userService.login(loginDto);
+        return Result.build(loginVo, ResultCodeEnum.SUCCESS);
+    }
+
+    //获取验证码图片
+    @GetMapping(value = "/generateValidateCode")
+    public Result<ValidateCodeVo> generateValidateCode() {
+        ValidateCodeVo validateCodeVo = validateCodeService.generateValidateCode();
+        return Result.build(validateCodeVo,ResultCodeEnum.SUCCESS);
+    }
+
+    //后端获取用户信息的接口就无需获取token，然后根据token从Redis中进行查询。可以直接从ThreadLocal中获取用户信息，然后进行返回。
+    @GetMapping("/getUserInfo")
+    public Result<User> getUserInfo(){
+        return Result.build(AuthContextUtil.get(),ResultCodeEnum.SUCCESS);
+    }
+}
